1d2763f1a2367c48228d9a303e9c9c0dc2a8982d
Kyaw Ko Ko Tun <89926955+KyawKoKoTun@users.noreply.github.com>
2023-03-12 13:08:08 +0630

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world! \n My brother is an idiot!'
+    return 'Hello world! \n<h1>My brother is an idiot!</h1>'
 
 
 if __name__ == '__main__':
